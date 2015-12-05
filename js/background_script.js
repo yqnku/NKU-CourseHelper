d1619857642de4b81bd99d16ced3d37e3f4ed8ec
Zhiqiang Ye <xiqian013@live.com>
2015-12-05 09:43:55 +0800

    Version 1.3

--- js/background_script.js
+++ js/background_script.js
@@ -57,12 +57,27 @@ function Search()
         //alert("课程名称："+message[0]+"\n"+"任课教师:"+message[1]+"\n"+"上课时间:"+message[2]+"\n"+"上课地点："+message[3]+"\n"+"开课单位："+message[4]);
 }
 
+//响应enter键
+ function EnterHandler()
+ {
+    var keyCode = event.keyCode ? event.keyCode : event.which ? event.which : event.charCode;
+    if (keyCode == 13)
+    {
+        var G = document.getElementById("query");
+        G.click()
+    }
+}
+
+
+
 function Load()
 {
 	var F = document.getElementById("sta");
 	F.onclick = AddFunCalc;
 	var G = document.getElementById("query");
 	G.onclick = Search;
+	var H = document.getElementById("xkxh");
+	H.onkeydown = EnterHandler;
 }
 window.onload = Load;
 
